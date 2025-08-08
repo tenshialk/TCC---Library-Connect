88e9142170526e028183d88b06637207b2ae3949
tenshialk <mariacicera0981@gmail.com>
2025-08-08 01:25:13 +0000

    22:25

--- app.py
+++ app.py
@@ -14,3 +14,19 @@ def login():
 @app.route('/cadastro')
 def cadastro():
     return render_template('cadastro.html')
+
+@app.route('/biblioteca')
+def biblioteca():
+    return render_template('biblioteca.html')
+
+@app.route('/perfil')
+def perfil():
+    return render_template('perfil.html')
+
+@app.route('/livro')
+def livro():
+    return render_template('livro.html')
+
+@app.route('/geral')
+def geral():
+    return render_template('geral.html')
